cebc9df151bb47c8ef48adfc16cbdacf4393f186
M Hightower <27247790+mhightower83@users.noreply.github.com>
2020-03-30 17:05:29 -0700

    Update StringEx.cpp

--- src/StringEx.cpp
+++ src/StringEx.cpp
@@ -1,7 +1,7 @@
 /**
  StringEx.cpp
 
- Copyright (c) 2017 Michael Hightower. All rights reserved.
+ Copyright (c) 2017-2020 Michael Hightower. All rights reserved.
 
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
